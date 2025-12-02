2e22c847826a098ec38f3eb75eaf649bc32445c9
daan.indebraekt_nsp <daan.indebraekt@ns.nl>
2025-12-02 18:16:40 +0100

    Day1 exercises opgelost met Fold, en wat herstructurering

--- src/problems/Day1.kt
+++ src/problems/Day1.kt
@@ -1,10 +1,12 @@
 package problems
 
 import Problem
+import problems.Dial.Companion.MAX
+import problems.Dial.Companion.rotatedBy
+import kotlin.Int
 import kotlin.math.abs
 
 class Dial {
-    val max: Int = 100
     var position: Int = 50
         private set
     var zeroesAfterRotations: Int = 0
@@ -12,13 +14,11 @@ class Dial {
     var allZeroClicks: Int = 0
         private set
 
-    fun getNewPosition(other: Int): Int = (position + other).mod(max)
+    fun rotate(rotation: Int) {
+        val newPosition = position rotatedBy rotation
 
-    fun rotate(other: Int) {
-        val newPosition = getNewPosition(other)
-
-        val fullRotations = abs((other / max))
-        val goesThroughZero = if (position + other.rem(max) !in 0..100 && position != 0) 1 else 0
+        val fullRotations = abs((rotation / MAX))
+        val goesThroughZero = if (position + rotation.rem(MAX) !in 0..100 && position != 0) 1 else 0
         val endsAtZero = if (newPosition == 0) 1 else 0
 
         position = newPosition
@@ -26,6 +26,11 @@ class Dial {
         allZeroClicks += fullRotations + goesThroughZero + endsAtZero
     }
 
+    companion object {
+        const val MAX: Int = 100
+
+        infix fun Int.rotatedBy(other: Int): Int = (this + other).mod(MAX)
+    }
 }
 
 class Day1 : Problem(1) {
@@ -33,22 +38,14 @@ class Day1 : Problem(1) {
         val problemInput = (if (test) testInput else input).map { it.parseLine() }
         solveAV1(problemInput)
         solveAV2(problemInput)
-        solveB(problemInput)
+        solveAV3(problemInput)
+        solveBV1(problemInput)
+        solveBV2(problemInput)
     }
 
     fun String.parseLine(): Int {
         val (dir, clicks) = Pair(this[0], this.slice(1..<this.length).toInt())
         return if (dir == 'L') -1 * clicks else clicks
-    }
-
-    fun solveAV2(input: List<Int>) = Dial().apply {
-        input.forEach { rotate(it) }
-        println("First half answer (version 2): $zeroesAfterRotations")
-    }
-
-    fun solveB(input: List<Int>) = Dial().apply {
-        input.forEach { rotate(it) }
-        println("Second half answer: $allZeroClicks")
     }
 
     fun solveAV1(input: List<Int>) {
@@ -59,12 +56,52 @@ class Day1 : Problem(1) {
             }
         val dialsAfterRotation = rotations
             .mapIndexed { index, _ -> rotations.slice(0..index).sum() }
+            .map { it.mod(100) }
 
         val rotationsEndingAtZero = dialsAfterRotation
-            .map { it.mod(100) }
+
             .filter { it == 0 }
             .size
+
         println("First half answer (version 1): $rotationsEndingAtZero")
+    }
+
+    fun solveAV2(input: List<Int>) = Dial().apply {
+        input.forEach { rotate(it) }
+        println("First half answer (version 2): $zeroesAfterRotations")
+    }
+
+    fun solveAV3(input: List<Int>) {
+        var rotationsEndingAtZero = 0
+        input.fold(initial = 50) { currentPosition, rotation ->
+            val newPosition = currentPosition rotatedBy rotation
+            if (newPosition == 0) rotationsEndingAtZero++
+            newPosition
+        }
+        println("First half answer (version 3): $rotationsEndingAtZero")
+    }
+
+    fun solveBV1(input: List<Int>) = Dial().apply {
+        input.forEach { rotate(it) }
+        println("Second half answer (version 1): $allZeroClicks")
+    }
+
+    fun solveBV2(input: List<Int>) {
+        var allZeroClicks = 0
+
+        input.fold(50) { currentPosition, rotation ->
+            val newPosition = currentPosition rotatedBy rotation
+
+            val fullRotations = abs((rotation / MAX))
+            val goesThroughZero = if (currentPosition + rotation.rem(MAX) !in 0..100 && currentPosition != 0) 1 else 0
+            val endsAtZero = if (newPosition == 0) 1 else 0
+
+            allZeroClicks += fullRotations + goesThroughZero + endsAtZero
+
+            newPosition
+        }
+
+        println("Second half answer (version 2): $allZeroClicks")
     }
 }
 
